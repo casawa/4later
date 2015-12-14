bb6e9db8ea36f40f481c08f10e606b26e63e95f7
Chaitanya Asawa <casawa@stanford.edu>
2015-12-14 02:23:22 -0800

    The Add button listens for clicks and adds the current URL to the saved URLs list. Still need to interact with local storage properly -- retrieval, addition, deletion. Also, need to have a deletion method + improve design a bit

--- popup.js
+++ popup.js
@@ -8,14 +8,50 @@ function getCurrentTabInfo(callback) {
   chrome.tabs.query(queryInfo, function(tabs) {
     var tab = tabs[0];
     var url = tab.url;
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-    callback(url);
+    var tabName = tab.title;
+    var faviconUrl = tab.favIconUrl;
+    console.assert(typeof url == 'string', 'tab.url should be a string'); // Based on Chromium Example
+    callback(url, tabName, faviconUrl);
   });
 }
 
+// Adds an item to the saved list HTML display (does not insert into storage)
+function addItemToList(url, tabName, faviconUrl) {
+  var savedList = document.getElementById('saved_list');
+  var item = document.createElement('li');
+  item.appendChild(document.createTextNode(url));
+  savedList.appendChild(item);
+}
+
+// Saves an item to storage
+function saveItemInStorage(url, tabName, faviconUrl) {
+  chrome.storage.sync.set({'saved_url': url}, function() {
+    var saveMessage = document.getElementById('saved_message');
+    saveMessage.style.display = 'block';
+  });
+}
+
+// Gets the saved websites
+function getSavedWebsites() {
+  // Fetch the websites
+  // For all websites, do addItemToList?
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabInfo(function(url) {
-    var current_website = document.getElementById('current');
-    current_website.innerHTML = "<b> Current Website: </b>" + url;
+
+  getSavedWebsites();
+
+  // Display current tab info
+  getCurrentTabInfo(function(url, tabName, faviconUrl) {
+    var currentWebsite = document.getElementById('current');
+    currentWebsite.innerHTML = "<b> Current Website: </b>" + tabName + " - " + url;
+  });
+
+  addButton = document.getElementById('add');
+  addButton.addEventListener('click', function() {
+    getCurrentTabInfo(function(url, tabName, faviconUrl) {
+      addItemToList(url, tabName, faviconUrl);
+      saveItemInStorage(url, tabName, faviconUrl);
+    });
   });
 });
